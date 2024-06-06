cc51c74539962743d7bffe6ced1453dd66403ecd
crissferro <crissferro@gmail.com>
2024-06-06 14:11:16 -0300

    se arma la estructura de la web

--- app.js
+++ app.js
@@ -29,9 +29,26 @@ server.listen(3000, () => console.log(`Servidor arriba en el puerto 3000`))
 //creo un server estatico con express (modulo externo)
 
 const express = require(`express`)
+const override = require('method-override')
+const rutas = require('./src/routes/adminRoutes.js')
+
 const app = express()
 
-const port = 3000 || process.env.port    //defino el puerto del servidor
+const port = 3000 || process.env.port || 8080   //defino el puerto del servidor
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(override('_metodo'))
+
+app.use('/', rutas)
+
+app.use((rew, res, next) => {
+    res.status(404).send(`<h1 style="color: red"> Recurso no encontrado!</h1>`)
+})
+
+app.listen(port, () => console.log(`Hola, estoy arriba en el puerto: ${port}`))
+
+
 
 //aparece por pantalla  el response
 /*
---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+
+router.get("/dinamic/:num", (req, res) => {
+    //console.log(req.params.num)
+    res.sendFile(path.resolve(__dirname + `./../views/dinamico${req.params.num}.html`))
+})
+
+router.post('/dinamic', (req, res) => {
+    //console.log(req.body)
+    res.send(`<h2>Se hizo algo con ${req.body.create} en el create</h2><a href="/dinamic/1">Regresar a la página anterior</a>`)
+    //res.json(req.body.create)
+})
+
+router.put('/dinamic', (req, res) => {
+    res.send(`<h2>Se hizo algo con ${req.body.actualizar} en el update</h2><a href="/dinamic/1">Regresar a la página anterior</a>`)
+})
+
+router.delete('/dinamic', (req, res) => {
+    res.send(`<h2>Se hizo algo con ${req.body.eliminar} en el delete</h2><a href="/dinamic/1">Regresar a la página anterior</a>`)
+})
+
+module.exports = router
