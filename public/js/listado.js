8fed293d0432d283c09625043f0de069ff0ce6cf
crissferro <crissferro@gmail.com>
2024-06-24 11:56:10 -0300

    modificando css de listado.html

--- public/js/listado.js
+++ public/js/listado.js
@@ -2,18 +2,32 @@ document.querySelector('body').onload = async () => {
 	const res = await fetch(`http://localhost:8080/listado`)
 	const datos = await res.json()
 	let listaHTML = document.querySelector(`#listado`)
-	listaHTML.innerHTML = ''
+	/*listaHTML.innerHTML = ''*/
+	listaHTML.innerHTML = `
+        <div class="list-header">
+            <h4>Nombre</h4>
+            <h4>Descripción</h4>
+            <h4>Precio</h4>
+            <h4>Tipo Producto</h4>
+            <h4>Marca</h4>
+            <h4>Acciones</h4>
+        </div>
+    `;
+
+
 	datos.forEach(registro => {
 		listaHTML.innerHTML += `
-		<form method="POST" action="/listado?_metodo=DELETE" style="display:flex">
-			<h4>${registro.nombre}</h4>
-			<h4>${registro.descripcion}</h4>
-			<h4>${registro.precio}</h4>
-            <h4>${registro.id_tipoProducto}</h4>
-			<h4>${registro.id_marca}</h4>
+		<form method="POST" action="/listado?_metodo=DELETE" class="list-item">
+			<h5>${registro.nombre}</h5>
+			<h5>${registro.descripcion}</h5>
+			<h5>${registro.precio}</h5>
+            <h5>${registro.id_tipoProducto}</h5>
+			<h5>${registro.id_marca}</h5>
 			<input type="hidden" name="idEliminar" value="${registro.id}">
+			<div class="actions">
 			<h4><button><a href="/modificar/${registro.id}">Modificar</a></h4>
 			<h4><input type="submit" value="Eliminar"></h4>
+			</div>
 		</form>`
 	})
 }
